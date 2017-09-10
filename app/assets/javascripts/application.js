0bcde9a129920ff73c6b33caa2cb8788dd9fadcb
Syaf Djamil <the.syaf@gmail.com>
2017-09-10 15:56:43 +1000

    resolved navgation issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,95 +17,144 @@
 
 $(document).ready(function(){
 
-  // change the views based on url parameter
-  function routes(url) {
-
-    switch (url) {
-      case "history":
-        viewHistoryPage();
-        break;
-      case "contact":
-        viewContactPage();
-        break;
-      case "projects":
-        viewProjectsPage();
-        break;
-      default:
-        viewHomepage();
-    }
-
-  }
-
-  function hashchanged(){
-    var hash = location.hash.replace( /^#/, '' );
-    routes(hash);
-  }
-
-  function viewHomepage() {
-  }
-
-  function viewHistoryPage() {
-    $('#sc-history').show('slide', {direction:'left'}, 700);
-    $('.navcontact').toggle('slide', {direction:'down'}, 700);
-    $('.navprojects').toggle('slide', {direction:'right'}, 700);
-    $('.navhistory').addClass('back');
-  }
-
-  function viewContactPage() {
-    $('#sc-contact').toggle('slide', {direction:'down'}, 700);
-    $('.navhistory').toggle('slide', {direction:'left'}, 700);
-    $('.navprojects').toggle('slide', {direction:'right'}, 700);
-    $('.navcontact').addClass('back');
-  }
-
-  function viewProjectsPage() {
-    $('#sc-projects').toggle('slide', {direction:'right'}, 700);
-    $('.navcontact').toggle('slide', {direction:'down'}, 700);
-    $('.navhistory').toggle('slide', {direction:'left'}, 700);
-    $('.navprojects').addClass('back');
-  }
-
-
-  $('.back').on('click touch', function(){
-    $(this).removeClass('back');
-    window.location.replace( /^#/, '' );
-  })
-
-  $(".navhistory:not(.back)").on('click touch', function () {
-    window.location.replace("#history");
-  });
-
-  $(".navcontact:not(.back)").on('click touch', function () {
-    window.location.replace("#contact");
-  });
-
-  $(".navprojects:not(.back)").on('click touch', function () {
-    window.location.replace("#projects");
-  });
-
-  // listen for url changes and fire the hash change function
-  $(window).on('hashchange', function(){
-    hashchanged();
-  }).trigger('hashchange');
+  // // change the views based on url parameter
+  // function routes(url) {
+  //
+  //   switch (url) {
+  //     case "history":
+  //       viewHistoryPage();
+  //       break;
+  //     case "contact":
+  //       viewContactPage();
+  //       break;
+  //     case "projects":
+  //       viewProjectsPage();
+  //       break;
+  //     default:
+  //       viewHomepage();
+  //   }
+  //
+  // }
+  //
+  // function hashchanged(){
+  //   var hash = location.hash.replace( /^#/, '' );
+  //   routes(hash);
+  // }
+  //
+  // function viewHomepage() {
+  // }
+  //
+  // function viewHistoryPage() {
+  //   $('#sc-history').show('slide', {direction:'left'}, 700);
+  //   $('.navcontact').toggle('slide', {direction:'down'}, 700);
+  //   $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  //   $('.navhistory').addClass('back');
+  // }
+  //
+  // function viewContactPage() {
+  //   $('#sc-contact').toggle('slide', {direction:'down'}, 700);
+  //   $('.navhistory').toggle('slide', {direction:'left'}, 700);
+  //   $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  //   $('.navcontact').addClass('back');
+  // }
+  //
+  // function viewProjectsPage() {
+  //   $('#sc-projects').toggle('slide', {direction:'right'}, 700);
+  //   $('.navcontact').toggle('slide', {direction:'down'}, 700);
+  //   $('.navhistory').toggle('slide', {direction:'left'}, 700);
+  //   $('.navprojects').addClass('back');
+  // }
+  //
+  //
+  // $('.back').on('click touch', function(){
+  //   $(this).removeClass('back');
+  //   window.location.replace( /^#/, '' );
+  // })
+  //
+  // $(".navhistory:not(.back)").on('click touch', function () {
+  //   window.location.replace("#history");
+  // });
+  //
+  // $(".navcontact:not(.back)").on('click touch', function () {
+  //   window.location.replace("#contact");
+  // });
+  //
+  // $(".navprojects:not(.back)").on('click touch', function () {
+  //   window.location.replace("#projects");
+  // });
+  //
+  // // listen for url changes and fire the hash change function
+  // $(window).on('hashchange', function(){
+  //   hashchanged();
+  // }).trigger('hashchange');
 
 
 // mobile nav
-$(".mnav-history").on('click touch', function () {
-  $('#sc-history').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-projects').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-contact').toggle('slide', {direction:'down'}, 700);
+$(".mnav-history, .navhistory").on('click touch', function () {
+  $('#sc-history').toggle('slide', {direction:'left'}, 700);
+  if ($(window).width() > 576){
+      $('.navcontact').toggle('slide', {direction:'down'}, 700);
+      $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
 });
 
-$(".mnav-projects").on('click touch', function () {
-  $('#sc-projects').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-history').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-contact').toggle('slide', {direction:'down'}, 700);
+$(".mnav-projects, .navprojects").on('click touch', function () {
+  $('#sc-projects').toggle('slide', {direction:'right'}, 700);
+  if ($(window).width() > 576){
+      $('.navcontact').toggle('slide', {direction:'down'}, 700);
+      $('.navhistory').toggle('slide', {direction:'left'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
 });
 
-$(".mnav-contact").on('click touch', function () {
+$(".mnav-contact, .navcontact").on('click touch', function () {
   $('#sc-contact').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-projects').toggle('slide', {direction:'down'}, 700);
-  $('.mnav-history').toggle('slide', {direction:'down'}, 700);
+  if ($(window).width() > 576){
+      $('.navhistory').toggle('slide', {direction:'left'}, 700);
+      $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
+});
+
+// close screen
+$(".close-projects").on('click touch', function () {
+  $('#sc-projects').toggle('slide', {direction:'right'}, 700);
+  if ($(window).width() > 576){
+      $('.navcontact').toggle('slide', {direction:'down'}, 700);
+      $('.navhistory').toggle('slide', {direction:'left'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
+});
+
+$(".close-contact").on('click touch', function () {
+  $('#sc-contact').toggle('slide', {direction:'down'}, 700);
+  if ($(window).width() > 576){
+      $('.navhistory').toggle('slide', {direction:'left'}, 700);
+      $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
+});
+
+$(".close-history").on('click touch', function () {
+  $('#sc-history').toggle('slide', {direction:'left'}, 700);
+  if ($(window).width() > 576){
+      $('.navcontact').toggle('slide', {direction:'down'}, 700);
+      $('.navprojects').toggle('slide', {direction:'right'}, 700);
+  }
+  else {
+    $('.mobile-menu').toggle('slide', {direction:'down'}, 700);
+  }
 });
 
 // easter egg
